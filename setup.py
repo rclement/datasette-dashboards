e57aa01cdccf1def5f425c68fa9b61343707bc62
Romain Clement <contact@romain-clement.net>
2021-04-25 00:10:52 +0200

    Add missing packages for test requirements in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=["datasette_dashboards"],
     entry_points={"datasette": ["dashboards = datasette_dashboards"]},
     install_requires=["datasette"],
-    extras_require={"test": ["pytest", "pytest-asyncio"]},
+    extras_require={"test": ["faker", "pytest", "pytest-asyncio", "sqlite-utils"]},
     tests_require=["datasette-my-plugin[test]"],
     package_data={"datasette_dashboards": ["templates/*.html", "static/*"]},
 )
