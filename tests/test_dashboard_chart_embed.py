dfe1b4b03ce6a14b02dd4e8c97a1be427a28522e
Romain Clement <contact@romain-clement.net>
2023-03-29 19:38:22 +0200

    Fix code format

--- tests/test_dashboard_chart_embed.py
+++ tests/test_dashboard_chart_embed.py
@@ -7,9 +7,10 @@ async def test_dashboard_chart_embed(datasette):
     for slug, dashboard in dashboards.items():
         for chart_slug, chart in dashboard["charts"].items():
             props = chart.keys()
-            response = await datasette.client.get(f"/-/dashboards/{slug}/{chart_slug}/embed")
+            response = await datasette.client.get(
+                f"/-/dashboards/{slug}/{chart_slug}/embed"
+            )
             assert response.status_code == 200
             assert f'<h1>{dashboard["title"]}</h1>' in response.text
             if "title" in props:
                 assert f'<p>{chart["title"]}</p>' in response.text
-
