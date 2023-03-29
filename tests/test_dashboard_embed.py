f8fba9c654cb2b8f243dbed5ea3e6d0f79d40016
Romain Clement <contact@romain-clement.net>
2023-03-29 19:25:42 +0200

    Add tests for dashboard and chart embed routes

---
+++ tests/test_dashboard_embed.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_dashboard_embed(datasette):
+    dashboards = datasette._metadata["plugins"]["datasette-dashboards"]
+    for slug, dashboard in dashboards.items():
+        response = await datasette.client.get(
+            f"/-/dashboards/{slug}/embed", follow_redirects=True
+        )
+        assert response.status_code == 200
+        assert f'<h1>{dashboard["title"]}</h1>' in response.text
+        assert f'<p>{dashboard["description"]}</p>' in response.text
+
+
+@pytest.mark.asyncio
+async def test_dashboard_embed_filters_default_redirect(datasette):
+    dashboards = datasette._metadata["plugins"]["datasette-dashboards"]
+    slug = list(dashboards.keys())[0]
+    response = await datasette.client.get(f"/-/dashboards/{slug}/embed")
+    assert response.status_code == 302
