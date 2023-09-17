76026475dee76d411a231fafbb6532918b7b25a9
Romain Clement <git@romain-clement.net>
2023-09-17 12:32:36 +0200

    Use native build artifacts to vendor npm modules

--- utils/bundle_js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/rollup node_modules/vega/build/vega.min.js --file datasette_dashboards/static/vega.min.js
-./node_modules/.bin/rollup node_modules/vega-embed/build/vega-embed.min.js --file datasette_dashboards/static/vega-embed.min.js
-./node_modules/.bin/rollup node_modules/vega-lite/build/vega-lite.min.js --file datasette_dashboards/static/vega-lite.min.js
